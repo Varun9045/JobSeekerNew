91102a743e5656896763c0e371e3932ee0afaf4d
varun <varunbaliyan123@gmail.com>
2022-12-12 22:18:29 +0530

    sdf

--- app/src/main/java/com/example/jobseekernew/IntroSlider.java
+++ app/src/main/java/com/example/jobseekernew/IntroSlider.java
@@ -3,6 +3,7 @@ package com.example.jobseekernew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroSlider extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class IntroSlider extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_slider);
+    }
+
+    public void next(View view) {
+    }
+
+    public void getStart(View view) {
     }
 }
