ee7971a214bef921e40f536138f1693fe27e725e
varun <varunbaliyan123@gmail.com>
2022-12-28 08:31:05 +0530

    commit 2

--- app/src/main/java/com/example/jobseekernew/MainActivity.java
+++ app/src/main/java/com/example/jobseekernew/MainActivity.java
@@ -7,22 +7,28 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN=4000;  // 2 sec
-//     handler;
+    private static int SPLASH_SCREEN=4000;  // 4 sec
+
+
+
+    //     handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  // call
 
-//        new Handler(); // object created in short form
+//        new Handler()..postDelayed(); // object created in short form
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 //                Intent intent = new Intent(MainActivity.this,IntroSlider.class);
 //                startActivity(intent);
-                startActivity(new Intent(MainActivity.this,IntroSlider.class));  // short form
+                startActivity(new Intent(MainActivity.this,IntroSlider.class));
+                finish();// short form
             }
         },SPLASH_SCREEN);
+
+
     }
 }
---
+++ app/src/main/java/com/example/jobseekernew/Recruiter/RecruiterLogin.java
@@ -0,0 +1,16 @@
+package com.example.jobseekernew.Recruiter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.jobseekernew.R;
+
+public class RecruiterLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recruiter_login);
+    }
+}
